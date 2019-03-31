763381568495c1447f5923be5f87a839be4e3979
AndreyLyahov <dneprdiagnost@gmail.com>
2019-03-31 14:12:23 +0300

    add login

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -1,9 +1,11 @@
 package com.akadatsky;
 
+import com.akadatsky.login.Login;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Login.getName();
     }
 
 }
--- src/com/akadatsky/login/Login.java
+++ src/com/akadatsky/login/Login.java
@@ -1,4 +1,9 @@
 package com.akadatsky.login;
 
 public class Login {
+    private static String name;
+
+    public static String getName() {
+        return name;
+    }
 }
