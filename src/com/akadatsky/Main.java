0a197912d20e265a01c92b7ba3e138d7ec15d7f9
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2019-03-31 14:08:57 +0300

    add getProperty

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -1,10 +1,13 @@
 package com.akadatsky;
 
+import com.akadatsky.settings.Settings;
+
 import com.akadatsky.login.Login;
 
 public class Main {
 
     public static void main(String[] args) {
+        Settings.getProperty();
         Login.getName();
     }
 
--- src/com/akadatsky/settings/Settings.java
+++ src/com/akadatsky/settings/Settings.java
@@ -1,4 +1,8 @@
 package com.akadatsky.settings;
 
 public class Settings {
+
+    public static void getProperty() {
+    }
+
 }
